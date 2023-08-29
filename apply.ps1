ff5a0236ba7ff01256f651f1852a99e37f32a14a
David <2019819305@qq.com>
2023-08-29 16:31:16 +0800

    zlh

--- apply.ps1
+++ apply.ps1
@@ -1,2 +1,2 @@
-Remove-Item D:/Code/Gitee/notes/md2html.exe
-Copy-Item -Path D:/Code/GitHub/md2html/md2html.exe -Destination D:/Code/Gitee/notes/md2html.exe
+Remove-Item D:/Software/David/bin/md2html.exe
+Copy-Item -Path D:/Code/GitHub/md2html/md2html.exe -Destination D:/Software/David/bin/md2html.exe
